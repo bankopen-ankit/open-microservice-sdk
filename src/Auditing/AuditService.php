632ed6d3c2c5879f0ed15490ff0252f77301b62b
Ankit Tiwari <ankit.tiwari@bankopen.co>
2022-10-29 13:04:21 +0530

    added few important files to setup base repo

---
+++ src/Auditing/AuditService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bankopen\OpenMicroserviceSdk\Auditing;
+
+class AuditService
+{
+    public function __construct()
+    {
+    }
+
+    private function createAudit()
+    {
+        return true;
+    }
+}
