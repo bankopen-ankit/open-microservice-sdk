7d5421395987ffe9427728ba03682b9c60ba0760
Ankit Tiwari <ankit.tiwari@bankopen.co>
2022-10-29 18:48:34 +0530

    added audit log and remote request file in to the sdk

--- src/Auditing/AuditService.php
+++ src/Auditing/AuditService.php
@@ -2,14 +2,55 @@
 
 namespace Bankopen\OpenMicroserviceSdk\Auditing;
 
+use Bankopen\OpenMicroserviceSdk\Utility\RemoteRequest;
+
+/**
+ * Class AuditService
+ *
+ * @author Ankit Tiwari <ankit.tiwari@bankopen.co>
+ */
 class AuditService
 {
-    public function __construct()
+    /**
+     * https://github.com/Architectural-Documents/audit-log/blob/main/000-audit-log-service.md#data-definition-m---mandatory-o---optional
+     * @var array
+     */
+    private $paramKeys = ['product', 'event_source', 'event_type', 'event_action', 'event_data', 'event_created_at'];
+    /**
+     * @var array
+     */
+    private $headerKeys = ['Authorization'];
+
+    /**
+     * @param $url
+     * @param $parameters
+     * @param $headers
+     * @return bool
+     * @throws \Exception
+     */
+    private function createAudit($url, $parameters, $headers)
     {
+        $this->validateKeys($this->paramKeys, $parameters);
+        $this->validateKeys($this->headerKeys, $headers);
+
+        $remote = (new RemoteRequest($url, $parameters, $headers))->post();
+
+        return true;
     }
 
-    private function createAudit()
+    /**
+     * @param $which
+     * @param $keys
+     * @throws \Exception
+     */
+    private function validateKeys($which, $keys)
     {
-        return true;
+        foreach ($keys as $key => $value) {
+            if (in_array($key, $which)) {
+                continue;
+            }
+
+            throw new \Exception($key . ' is missing');
+        }
     }
 }
